3f95435f7330ede8e21e03d965a19b2a009b1d84
tommygomez25 <maysan54321@gmail.com>
2022-05-16 19:44:05 +0100

    idk

--- addmx.c
+++ addmx.c
@@ -7,6 +7,7 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <math.h>
 
 
 #define BUFFER_SIZE 3
@@ -17,71 +18,68 @@ void print_usage(const char *exe) {
 
 int addmx(const char *file1, const char *file2){
 
-    int f1 = open(file1,O_RDONLY,S_IRUSR |S_IWUSR);
+    FILE *f1 = fopen(file1,"r");
+
     if (f1 == NULL){
         perror("Error in opening file 1");
         return -1;
     }
-    int f2 = open (file2, O_RDONLY,S_IRUSR |S_IWUSR);
+
+    FILE *f2 = fopen (file2, "r");
+
     if (f2 == NULL){
         perror("Error in opening file 2");
         return -1;
     }
 
-    struct stat sb;
-    if (fstat(f1,&sb) == -1){
-        perror("Couldn't get file size.\n");
+
+    char ch;
+    unsigned row,col;
+    size_t len = 0;
+    char *line = NULL;
+    getline(&line,&len,f1);
+    sscanf(line,"%d%c%d",&row,&ch,&col);
+    printf("%d\n",row);
+    printf("%d\n",col);
+
+
+    char  *file_in_memory = mmap(NULL,row*col*2, PROT_READ, MAP_SHARED, fileno(f1), 0);
+    if (file_in_memory == MAP_FAILED){
+        printf("mapeamento failed\n");
+        return 1;
     }
-    printf("The file size is %ld\n",sb.st_size);
 
-    struct stat sb2;
-    if (fstat(f1,&sb2) == -1){
-        perror("Couldn't get file size.\n");
-    }
-    printf("The file size is %ld\n",sb2.st_size);
-
-    char *file_in_memory = mmap(NULL,sb.st_size ,PROT_READ ,MAP_SHARED,f1,0);
-    char *file_in_memory2 = mmap(file_in_memory,sb2.st_size*2,PROT_READ,MAP_SHARED,f2,0);
-    char *file_in_memory3 = mmap(file_in_memory2,sb.st_size * 2,PROT_WRITE,MAP_SHARED,STDOUT_FILENO,0);
-
-    for (int i = 0; i < sb.st_size;i++){
-        printf("%c",file_in_memory[i]);
-    }
-    for (int i = 0; i < sb2.st_size;i++){
-        printf("%c",file_in_memory2[i]);
-    }
-    unsigned int row = atoi(&file_in_memory[0]) ;
-    unsigned int col = atoi(&file_in_memory[2]) ;
-    //file_in_memory3[0] = row + '0'; file_in_memory3[1] = 'x'; file_in_memory3[2] = col + '0';
-
-    pid_t pid_matrix[col];
-
-    for (size_t i = 1; i <= col;i++) {
-        pid_matrix[col] = fork();
-        if (pid_matrix[col] == -1) {
-            //erro
+    for (int i = 0; i < row*col;i++){
+        /*
+        if (file_in_memory[i+1] == ' ' || file_in_memory[i+1] == '\n' || file_in_memory[i+1] == 'x'){
+            i++;
         }
-        else if (pid_matrix[col] == 0) {
-            //child process
-
-            for (int j = 1; j <= row;j++){
-                int new_col = atoi(&file_in_memory[4 + (j-1)*(col*2) + ((i-1)*2)] )+ atoi(&file_in_memory2[4 + (j-1)*(col*2) + ((i-1)*2)]);_
-                printf("%d\n",new_col);
-            }
-
-            printf("Child process with pid %d for column %d\n", getpid(),i);
-            exit(EXIT_SUCCESS);
-        }
-        else {
-            //parent process
-            printf("Created a new process with pid %d\n", pid_matrix[col]);
-        }
+         */ 
+        printf("[%d] : %d\n",i,atoi(&file_in_memory[i]));
     }
+    int resultado = atoi(&file_in_memory[4]) + atoi(&file_in_memory[5]);
+    printf("%d",resultado);
+    /*
+    int i = 0;
+    do{
+        ch = fgetc(f1);
+        if (feof(f1)){
+            break;
+        }
 
-    munmap(file_in_memory,sb.st_size);
-    munmap(file_in_memory2,sb2.st_size);
-    close(f1);
-    close(f2);
+        if ( ch == ' '){
+            continue;
+        }
+        printf("%c",ch);
+        i++;
+    } while(1);*/
+
+
+    fclose(f1);
+    int err = munmap(file_in_memory,row*col);
+    if ( err!=0){
+        printf("cenas");
+    }
     return 0;
 }
 
