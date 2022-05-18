95ef032155426581c924bdc408bcdc9c0a0e07d5
tommygomez25 <maysan54321@gmail.com>
2022-05-18 17:19:02 +0100

    q2 done

--- addmx.c
+++ addmx.c
@@ -31,30 +31,18 @@ int addmx(const char *file1, const char *file2){
         return -1;
     }
 
-    struct stat sb;
-    if (fstat(fileno(f1),&sb) == -1){
-        perror("Couldn't get file size.\n");
-    }
-    printf("The file size is %ld\n",sb.st_size);
-
-    struct stat sb2;
-    if (fstat(fileno(f2),&sb2) == -1){
-        perror("Couldn't get file size.\n");
-    }
-    printf("The file size is %ld\n",sb2.st_size);
-
     char ch;
     unsigned row,col;
     size_t len = 0;
     char *line = NULL;
     getline(&line,&len,f1);
     sscanf(line,"%d%c%d",&row,&ch,&col); // vai buscar a primeira linha para saber quantas linhas e colunas tem a matriz
-    printf("row : %d\n",row);
-    printf("col : %d\n",col);
 
-    int * map =(int *) mmap(NULL,row*col,PROT_READ | PROT_WRITE,MAP_SHARED | MAP_ANONYMOUS,-1,0);
+
     // MAP_ANONYMOUS faz com que o argumento fd seja ignorado, ou seja, ele não vai buscar file nenhum para mapear, simplesmente aloca memória onde os blocos ficam empty
+    int * map =(int *) mmap(NULL,row*col,PROT_READ | PROT_WRITE,MAP_SHARED | MAP_ANONYMOUS,-1,0);
     int * map2 = (int *) mmap(map,row*col,PROT_READ | PROT_WRITE,MAP_SHARED|MAP_ANONYMOUS,-1,0);
+    int * map3 = (int *) mmap(map2,row*col,PROT_WRITE,MAP_SHARED|MAP_ANONYMOUS,-1,0);
 
     int index = 0;
     int num;
@@ -63,55 +51,56 @@ int addmx(const char *file1, const char *file2){
         index++;
     }
 
-    for (int i = 0; i < row*col;i++){
-        printf("%d\n",map[i]);
-    }
-
-    int counter = 0;
     getline(&line,&len,f2); // para ignorar a primeira linha do segundo ficheiro
     index = 0;
     while (fscanf(f2,"%d",&num) != EOF){
-        printf("%d",num);
+        //printf("%d",num);
         map2[index] = num;
         index++;
     }
-    /*
-    for (int i = 0; i < row*col;i++){
-        printf("%d\n",map2[i]);
-    }
-    */
-    /*
+
     pid_t pid_matrix[col];
 
-    for (size_t i = 1; i <= col;i++) {
+    for (int i = 0; i <= col;i++) {
         pid_matrix[col] = fork();
+        int aux = i;
         if (pid_matrix[col] == -1) {
             //erro
         }
         else if (pid_matrix[col] == 0) {
             //child process
 
-            for (int j = 1; j <= row;j++){
-                int new_col = atoi(&file_in_memory[counter + (j-1)*(col*2) + ((i-1)*2)] )+ atoi(&file_in_memory2[counter + (j-1)*(col*2) + ((i-1)*2)]);
-                file_in_memory3[counter+ (j-1)*(col*2) + ((i-1)*2)] = new_col + '0';
-                printf("%d\n",new_col);
+            for (int j = 0; j < row;j++){
+                map3[aux] = map[aux] + map2[aux];
+                aux+= col;
             }
-
-            printf("Child process with pid %d for column %d\n", getpid(),i);
+            //printf("Child process with pid %d for column %d\n", getpid(),i);
             exit(EXIT_SUCCESS);
         }
         else {
             //parent process
-            printf("Created a new process with pid %d\n", pid_matrix[col]);
+
+            //printf("Created a new process with pid %d\n", pid_matrix[col]);
         }
     }
 
-    for ( int i = 0; i < sb.st_size;i++){
-        printf("%c",file_in_memory3[i]);
+    printf("%dx%d\n",row,col);
+
+    int i = 0;
+    while (1) {
+        printf("%d ",map3[i]);
+        if ( (i+1) % col == 0){
+            printf("\n");
+        }
+        if ( i+1 == row*col){
+            break;
+        }
+        i++;
     }
-    munmap(file_in_memory,sb.st_size);
-    munmap(file_in_memory2,sb2.st_size);
-     */
+
+    munmap(map,row*col);
+    munmap(map2,row*col);
+    munmap(map3,row*col);
     close(fileno(f1));
     close(fileno(f2));
     return 0;
